02333323413d9259ab97246ab0d7318f7829fbc7
Ye Myo Aung <yemyo994@gmail.com>
2021-11-26 19:54:05 +0630

    Update extension.dart

--- lib/src/extension.dart
+++ lib/src/extension.dart
@@ -1,6 +1,6 @@
 part of starlight_thread;
 
-extension _StarlightThreadExtension<R, T> on StarlightThread {
+extension StarlightThreadExtension<R, T> on StarlightThread {
   String get _debugName =>
       "${_dateTime.year}${_dateTime.month}${_dateTime.day}${_dateTime.hour}${_dateTime.minute}${_dateTime.second}";
 
